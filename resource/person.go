69dd1db18eb49b232af64838991eedcf77887e90
K Samuel <korneliussamuel@gmail.com>
2018-10-07 21:41:44 +0700

    Add FindPersonByID

--- resource/person.go
+++ resource/person.go
@@ -1,8 +1,6 @@
 package resource
 
-import (
-	"database/sql"
-)
+import "database/sql"
 
 type Person struct {
 	Id   string `json:"id"`
@@ -14,4 +12,23 @@ func (p *Person) Save(db *sql.DB) {
 	if _, err := db.Exec("INSERT INTO persons (id, name, age) VALUES ($1, $2, $3)", p.Id, p.Name, p.Age); err != nil {
 		panic(err)
 	}
+}
+
+func FindPersonByID(db *sql.DB, id string) *Person {
+	rows, err := db.Query("SELECT * FROM persons WHERE id=$1", id)
+	if err != nil {
+		return nil
+	}
+	defer rows.Close()
+
+	p := Person{}
+	for rows.Next() {
+		if err := rows.Scan(&p.Id, &p.Name, &p.Age); err != nil {
+			return nil
+		}
+
+		return &p
+	}
+
+	return nil
 }
