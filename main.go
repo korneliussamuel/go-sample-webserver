89412e4aaaeeb011c93304ab5769bea30e34b713
K Samuel <korneliussamuel@gmail.com>
2018-10-07 15:17:28 +0700

    Create empty handlers to handle POST, GET, other methods

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		postHandler(w, r)
+	case http.MethodGet:
+		getHandler(w, r)
+	default:
+		defaultHandler(w, r)
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+}
